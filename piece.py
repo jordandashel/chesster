c7603c053071d2eccc767e7778a57c44f2c9bc95
Jordan Dashel <jordan.dashel@gmail.com>
2019-03-10 15:05:31 -0400

    add b/w to ASCII piece output

--- piece.py
+++ piece.py
@@ -9,38 +9,38 @@ class Piece:
     def __init__(self):
         self.name = ''
         self.is_white = True
-        self.queen_side = True
 
     def abv_name(self, in_unicode=True):
+        color = 'w' if self.is_white else 'b'
         if self.name == self.PAWN:
             if in_unicode:
                 return '\u2659 ' if self.is_white else '\u265F '
             else:
-                return 'P '
+                return color + 'P '
         elif self.name == self.ROOK:
             if in_unicode:
                 return '\u2656 ' if self.is_white else '\u265C '
             else:
-                return 'R '
+                return color + 'R '
         elif self.name == self.KNIGHT:
             if in_unicode:
                 return '\u2658 ' if self.is_white else '\u265E '
             else:
-                return 'Kt'
+                return color + 'Kt'
         elif self.name == self.BISHOP:
             if in_unicode:
                 return '\u2657 ' if self.is_white else '\u265D '
             else:
-                return 'B '
+                return color + 'B '
         elif self.name == self.QUEEN:
             if in_unicode:
                 return '\u2655 ' if self.is_white else '\u265B '
             else:
-                return 'Q '
+                return color + 'Q '
         elif self.name == self.KING:
             if in_unicode:
                 return '\u2654 ' if self.is_white else '\u265A '
             else:
-                return 'K '
+                return color + 'K '
         else:
-            return '  ' 
+            return '   ' 
