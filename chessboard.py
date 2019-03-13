cf577ad1fd9290318ccd7784a39f7f40f653bdd5
Jordan Dashel <jordan.dashel@gmail.com>
2019-03-13 00:42:37 -0400

    correct WvB representation

--- chessboard.py
+++ chessboard.py
@@ -2,7 +2,7 @@ from piece import Piece
 from square import Square
 
 
-board_files = [
+BOARD_FILES = [
    #('file', File's Piece, Queen's Side?)
     ('a', Piece.ROOK, True),
     ('b', Piece.KNIGHT, True),
@@ -14,15 +14,17 @@ board_files = [
     ('h', Piece.ROOK, False)
 ]
 
-board_ranks = [i for i in range(1, 9)]
+BOARD_RANKS = [i for i in range(1, 9)]
 
 class Chessboard:
+
     board = {}
+
     def __init__(self):
         last_square_white = True
-        for file_ in board_files:
+        for file_ in BOARD_FILES:
             self.board[file_[0]] = {}
-            for rank in board_ranks:
+            for rank in BOARD_RANKS:
                 self.board[file_[0]][rank] = Square(
                         Square.WHITE if not last_square_white 
                         else Square.BLACK
@@ -31,30 +33,37 @@ class Chessboard:
             last_square_white = not last_square_white
 
     def __str__(self):
-        board = '\n'
-        board += '  +'
-        for i in range(len(board_files)):
-            board += '---+'
-        board += '\n'
-        for rank in board_ranks:
-            board += str(rank) + ' '
-            for board_file in board_files:
-                bf = board_file[0]
-                board += '|'
-                piece_in_square = self.board[bf][rank]
-                if self.board[bf][rank] is not None:
-                    board += self.board[bf][rank].abv_name(in_unicode=False)
-                else:
-                    board += '   '
-            board += '|'
-            board += '\n'
-            board += '  +'
-            for i in range(len(board_files)):
-                board += '---+'
-            board += '\n'
+        # Basic building blocks of ASCII output
+        EMPTY_SQUARE = '   '
+        VERT_BARRIER = '|'
+        HORIZ_BORDER = '  +'
+        for i in range(len(BOARD_FILES)):
+            HORIZ_BORDER += '---+'
+        HORIZ_BORDER += '\n'
 
-        board += '  '
-        for f in board_files:
+        board = '\n'
+        board += HORIZ_BORDER
+
+        # to display white at the bottom
+        ranks = BOARD_RANKS.__reversed__()
+
+        for rank in ranks:
+            board += str(rank) + ' '
+            for board_file in BOARD_FILES:
+                file_ = board_file[0]
+                board += VERT_BARRIER
+                piece_in_square = self.square(file_, rank).piece
+                if piece_in_square is not None:
+                    board += piece_in_square.abv_name(in_unicode=False)
+                else:
+                    board += EMPTY_SQUARE
+            # board += '|'
+            board += VERT_BARRIER
+            board += '\n'
+            board += HORIZ_BORDER
+
+        board += EMPTY_SQUARE
+        for f in BOARD_FILES:
             board += '  ' + f[0] + ' '
 
         return board
@@ -62,29 +71,40 @@ class Chessboard:
     def square(self, rank, file_):
         return self.board[rank][file_]
 
+    def all_pieces(self, name, color):
+        all_pieces = []
+        for file_ in BOARD_FILES:
+            for rank in BOARD_RANKS:
+                piece = self.board[file_[0]][rank].piece
+                if piece and piece.name == name and piece.color == color:
+                    all_pieces.append(((file_[0], rank), piece))
+        return all_pieces
+
+
+
     def set_board(self):
-        for board_file in board_files:
-            bf = board_file[0]
+        for board_file in BOARD_FILES:
+            file_ = board_file[0]
             white_pawn = Piece()
-            white_pawn.is_white = True
+            white_pawn.color = Piece.WHITE
             white_pawn.name = Piece.PAWN
-            self.square(bf, 2).piece = white_pawn
+            self.square(file_, 2).piece = white_pawn
 
-        for board_file in board_files:
-            bf = board_file[0]
+        for board_file in BOARD_FILES:
+            file_ = board_file[0]
             black_pawn = Piece()
-            black_pawn.is_white = False
+            black_pawn.color = Piece.BLACK
             black_pawn.name = Piece.PAWN
-            self.square(bf, 7).piece = black_pawn
+            self.square(file_, 7).piece = black_pawn
 
-        for board_file in board_files:
-            bf = board_file[0]
+        for board_file in BOARD_FILES:
+            file_ = board_file[0]
             white_piece = Piece()
-            white_piece.is_white = True
+            white_piece.color = Piece.WHITE
             white_piece.name = board_file[1]
-            self.square(bf, 1).piece = white_piece
+            self.square(file_, 1).piece = white_piece
 
             black_piece = Piece()
-            black_piece.is_white = False
+            black_piece.color = Piece.BLACK
             black_piece.name = board_file[1]
-            self.square(bf, 8).piece = black_piece
+            self.square(file_, 8).piece = black_piece
