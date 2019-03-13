1c49ee6c41ddb21f6ab2eecd49181cd13552b966
Jordan Dashel <jordan.dashel@gmail.com>
2019-03-13 00:43:54 -0400

    update  to reflect new WvB standard

--- piece.py
+++ piece.py
@@ -1,4 +1,7 @@
 class Piece:
+    WHITE = 'w'
+    BLACK = 'b'
+
     PAWN = 'pawn'
     ROOK = 'rook'
     KNIGHT = 'knight'
@@ -8,38 +11,40 @@ class Piece:
 
     def __init__(self):
         self.name = ''
-        self.is_white = True
+        self.color = ''
+
+    def is_white(self):
+        return True if self.color == self.WHITE else False
 
     def abv_name(self, in_unicode=True):
-        color = 'w' if self.is_white else 'b'
         if self.name == self.PAWN:
             if in_unicode:
-                return '\u2659 ' if self.is_white else '\u265F '
+                return '\u2659 ' if self.is_white() else '\u265F '
             else:
                 return color + 'P '
         elif self.name == self.ROOK:
             if in_unicode:
-                return '\u2656 ' if self.is_white else '\u265C '
+                return '\u2656 ' if self.is_white() else '\u265C '
             else:
                 return color + 'R '
         elif self.name == self.KNIGHT:
             if in_unicode:
-                return '\u2658 ' if self.is_white else '\u265E '
+                return '\u2658 ' if self.is_white() else '\u265E '
             else:
                 return color + 'Kt'
         elif self.name == self.BISHOP:
             if in_unicode:
-                return '\u2657 ' if self.is_white else '\u265D '
+                return '\u2657 ' if self.is_white() else '\u265D '
             else:
                 return color + 'B '
         elif self.name == self.QUEEN:
             if in_unicode:
-                return '\u2655 ' if self.is_white else '\u265B '
+                return '\u2655 ' if self.is_white() else '\u265B '
             else:
                 return color + 'Q '
         elif self.name == self.KING:
             if in_unicode:
-                return '\u2654 ' if self.is_white else '\u265A '
+                return '\u2654 ' if self.is_white() else '\u265A '
             else:
                 return color + 'K '
         else:
