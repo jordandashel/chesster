f0fd4bffbeab538c07e710efb2cf06e22ca88e3b
Jordan Dashel <jordan.dashel@gmail.com>
2019-03-15 00:51:46 -0400

    Implement the motion objects.
    Highly persnickety isomorphism -- lots of very specific rules pertaining
    to each individual piece. This is a perfectly O.K. implementation but I
    know there is an elegant solution lurking under the surface. To be
    found.

--- motion.py
+++ motion.py
@@ -2,23 +2,18 @@
 #
 #   MOTION LANGUAGE:
 #       
-#       The chess board can be described like the first quarter of a
-#       cartesian plane, and insted of `x` & `y`, I use North and West.
+#       Files/ranks are largely ignored at this level. It is much
+#       simpler to convert to cartesian coordinates and work with
+#       those.
 #
-#       There are 2 Cardinal Directions: N, W
-#       There are 2 Other Directions: NW, NE
-#
-#       This means a direction like SE is expressed as negative NW.
-#
-#       These directions can be used to describe all motion on the
-#       chess board. In addition to direction, the motion vector needs
-#       magnitude. This can be expressed with an integer, '2', to 
-#       express a number of squares of movement or with a glob, '*', 
-#       to express unlimited motion in a direction.
+#       I /know/ there is an elegant regexe-esque language to be
+#       developed here but that is for another day.
+#       
 #
 ######################################################################
 
 import re
+import math
 
 # DUPLICATION -- find out where it belongs!!
 files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -28,28 +23,6 @@ ranks = [i for i in range(1, 9)]
 class Motion:
     def __init__(self):
        pass
-
-    def further_north(rank_1, rank_2):
-        return rank_1 > rank_2
-
-    def further_south(rank_1, rank_2):
-        return rank_1 < rank_2
-
-    def further_west(file_1, file_2):
-        for f in files:
-            if f == file_1 and not f == file_2:
-                return True
-            if f == file_2 and not f == file_1:
-                return False
-        return False
-
-    def further_east(file_1, file_2):
-        for f in files:
-            if f == file_2 and not f == file_1:
-                return True
-            if f == file_1 and not f == file_2:
-                return False
-        return False
     
     def indexed_file(file_):
         i = 0
@@ -60,88 +33,151 @@ class Motion:
     def filed_index(i):
         return files[i]
 
-    def valid_move(start, end):
-        return True
-   
-#MovementInterpreter?
-class MotionLanguage:
-    def __init__(self, motion_string):
-        rexp = r'([A-Z])\(([+-]*)\)([0-9]\*)'
-        pattern = re.compile(rexp)
-        direction, pos_neg, magnitude = pattern.match(motion_string)
+    def convert_to_cart_coords(file_rank):
+        file_, rank = file_rank
+        x = Motion.indexed_file(file_)
+        y = int(rank) - 1
+        return x, y
 
+    def convert_to_notation(x, y):
+        file_ = Motion.filed_index(x)
+        rank = y + 1
+        return file_, rank
 
 class RookMotion(Motion):
     # allowable_movement = ('N(+/-)*', 'W(+/-)*')
     # capturing_movement = allowable_movement
     def valid_move(start, destination):
-        start_file, start_rank = start
-        destination_file, destination_rank = destination
-        if start_file == destination_file:
-            return True
-        elif start_rank == destination_rank:
-            return True
-        else:
-            return False
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
+
+        return x_1 == x_2 or y_1 == y_2
 
     def intermediate_squares(start, destination):
-        intermediate_squares = []
-        if not Motion.valid_move(start, destination):
+        if not RookMotion.valid_move(start, destination):
             return None
 
-        start_file, start_rank = start
-        destination_file, destination_rank = destination
-        if Motion.further_north(start_rank, destination_rank):
-            start_rank -= 1
-            while start_rank >= destination_rank:
-                intermediate_squares.append(
-                        (start_file, start_rank))
-        elif Motion.further_south(start_rank, destination_rank):
-            start_rank += 1
-            while start_rank <= destination_rank:
-                intermediate_squares.append((start_file, start_rank))
-                start_rank += 1
-        elif Motion.further_west(start_file, destination_file):
-            indexed_start_file = Motion.indexed_file(start_file) + 1
-            indexed_destination_file = Motion.indexed_file(destination_file)
-            while indexed_start_file <= indexed_destination_file:
-                intermediate_squares.append((
-                    Motion.filed_index(indexed_start_file), start_rank))
-                indexed_start_file += 1
-        elif Motion.further_east(start_file, destination_file):
-            indexed_start_file = Motion.indexed_file(start_file) - 1
-            indexed_destination_file = Motion.indexed_file(destination_file)
-            while indexed_start_file >= indexed_destination_file:
-                intermediate_squares.append((
-                    Motion.filed_index(indexed_start_file), start_rank))
-                indexed_start_file -= 1
-        return intermediate_squares
+        assert RookMotion.valid_move(start, destination)
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
 
+        intermediate_sqs = []
 
+        # vertical mvmt
+        if x_1 == x_2:
+            direction = 1 if y_1 < y_2 else -1
+            y_1 += direction
+            # y_2 += direction
+            for i in range(y_1, y_2, direction):
+                intermediate_sqs.append(
+                        Motion.convert_to_notation(x_1, i))
+        # lateral mvmt
+        else:
+            direction = 1 if x_1 < x_2 else -1
+            x_1 += direction
+            # x_2 += direction
+            for i in range(x_1, x_2, direction):
+                intermediate_sqs.append(
+                        Motion.convert_to_notation(i, y_1))
+
+        return intermediate_sqs
 
             
 class KnightMotion(Motion):
-    allowable_movement = ('N(+/-)2,W(+/-)1', 'W(+/-)2,N(+/-)1')
-    capturing_movement = allowable_movement
+    # allowable_movement = ('N(+/-)2,W(+/-)1', 'W(+/-)2,N(+/-)1')
+    # capturing_movement = allowable_movement
+    def valid_move(start, destination):
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
+        if math.fabs(x_1 - x_2) == 1 and math.fabs(y_2 - y_1) == 2:
+            return True
+        if math.fabs(x_1 - x_2) == 2 and math.fabs(y_2 - y_1) == 1:
+            return True
+        return False
+
+    def intermediate_squares(start, destination):
+        assert KnightMotion.valid_move(start, destination)
+        return []
 
 class BishopMotion(Motion):
-    allowable_movement = ('NW(+/-)*', 'NE(+/-)*')
-    capturing_movement = allowable_movement
+    def valid_move(start, destination):
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
+
+        return math.fabs(x_1 - x_2) == math.fabs(y_1 - y_2)
+
+    def intermediate_squares(start, destination):
+        assert BishopMotion.valid_move(start, destination)
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
+
+        intermediate_sqs = []
+        intermediate_x = []
+
+        if x_1 > x_2:
+            x_2 += 1 # don't report destination square
+            while x_1 > x_2:
+                x_1 -= 1
+                intermediate_x.append(x_1)
+        else:
+            x_2 -= 1 # don't report destination square
+            while x_1 < x_2:
+                x_1 += 1
+                intermediate_x.append(x_1)
+
+        if y_1 > y_2:
+            for x in intermediate_x:
+                y_1 -= 1
+                sq = Motion.convert_to_notation(x, y_1)
+                intermediate_sqs.append(sq)
+        else:
+            for x in intermediate_x:
+                y_1 += 1
+                sq = Motion.convert_to_notation(x, y_1)
+                intermediate_sqs.append(sq)
+
+        return intermediate_sqs
+                
 
 class QueenMotion(Motion):
-    allowable_movement = ('N(+/-)*', 'W(+/-)*', 'NW(+/-)*', 'NE(+/-)*')
-    capturing_movement = allowable_movement
+    # allowable_movement = ('N(+/-)*', 'W(+/-)*', 'NW(+/-)*', 'NE(+/-)*')
+    # capturing_movement = allowable_movement
+    def valid_move(start, destination):
+        return RookMotion.valid_move(start, destination) \
+                or BishopMotion.valid_move(start, destination)
+
+    def intermediate_squares(start, destination):
+        if RookMotion.valid_move(start, destination):
+            return RookMotion.intermediate_squares(start, destination)
+        elif BishopMotion.valid_move(start, destination):
+            return BishopMotion.intermediate_squares(start, destination)
+        else:
+            return None
 
 class KingMotion(Motion):
-    allowable_movement = ('N(+/-)1', 'W(+/-)1', 'NW(+/-)1', 'NE(+/-)1')
-    capturing_movement = allowable_movement
+    # allowable_movement = ('N(+/-)1', 'W(+/-)1', 'NW(+/-)1', 'NE(+/-)1')
+    # capturing_movement = allowable_movement
+    def valid_move(start, destination):
+        return QueenMotion.valid_move(start, destination) \
+          and len(QueenMotion.intermediate_squares(start, destination)) == 0
+
+    # King never has intermediate sqs bc she can only move one sq
+    def intermediate_squares(start, destination):
+        assert KingMotion.valid_move(start, destination)
+        return []
 
 class PawnMotion(Motion):
-    pass
-    # if first_move:
-    #     allowable_movement = ('N(+)1', 'N(+)2')
-    # else:
-    #     allowable_movement = ('N(+)1')
-
-    # capturing_movement = ('N(+)1,W(+/-)1')
+    def valid_move(start, destination, first_move=False, capture=False, en_passant=False):
+        x_1, y_1 = Motion.convert_to_cart_coords(start)
+        x_2, y_2 = Motion.convert_to_cart_coords(destination)
+        if capture:
+            return y_1 + 1 == y_2 and (x_1 + 1 == x_2 or x_1 - 1 == x_2)
+        if first_move:
+            return y_1 + 1 == y_2 or y_1 +2 == y_2 and x_1 == x_2
+        elif en_passant:
+            # TO BE IMPLEMENTED
+            # not sure if it even belongs here yet but ???
+            pass
+        elif not first_move:
+            return x_1 == x_2 and y_1 + 1 == y_2
 
--- tests/test_motion.py
+++ tests/test_motion.py
@@ -2,29 +2,6 @@ import unittest
 from motion import *
 
 class MotionTestCase(unittest.TestCase):
-    def test_motion(self):
-        # motion = RookMotion()
-        can_move_to_square = RookMotion.valid_move('a1', 'a8')
-        self.assertTrue(can_move_to_square)
-        can_move_to_square = RookMotion.valid_move('a1', 'h8')
-        self.assertFalse(can_move_to_square)
-
-    def test_further_north(self):
-        self.assertFalse(Motion.further_north(1, 8))
-        self.assertTrue(Motion.further_north(4, 2))
-
-    def test_further_south(self):
-        self.assertFalse(Motion.further_south(8, 1))
-        self.assertTrue(Motion.further_south(2, 4))
-        
-    def test_further_west(self):
-        self.assertTrue(Motion.further_west('a', 'h'))
-        self.assertFalse(Motion.further_west('e', 'd'))
-
-    def test_further_east(self):
-        self.assertFalse(Motion.further_east('a', 'h'))
-        self.assertTrue(Motion.further_east('e', 'd'))
-
     def test_indexed_file(self):
         self.assertEqual(0, Motion.indexed_file('a'))
         self.assertEqual(7, Motion.indexed_file('h'))
@@ -33,6 +10,15 @@ class MotionTestCase(unittest.TestCase):
         self.assertEqual('a', Motion.filed_index(0))
         self.assertEqual('h', Motion.filed_index(7))
 
+    def test_convert_to_cart_coords(self):
+        self.assertEqual((0, 0), Motion.convert_to_cart_coords('a1'))
+        self.assertEqual((3, 6), 
+                Motion.convert_to_cart_coords(('d', 7)))
+
+    def test_convert_to_notation(self):
+        self.assertEqual(('a', 1), Motion.convert_to_notation(0, 0))
+
+class RookMotionTestCase(unittest.TestCase):
     def test_rook_intermediate_squares(self):
         interm_sqs = RookMotion.intermediate_squares(('a', 1), ('a', 8))
         self.assertEqual(interm_sqs, [
@@ -41,8 +27,7 @@ class MotionTestCase(unittest.TestCase):
             ('a', 4),
             ('a', 5),
             ('a', 6),
-            ('a', 7),
-            ('a', 8)
+            ('a', 7)
         ])
         interm_sqs = RookMotion.intermediate_squares(('a',1), ('g', 1))
         self.assertEqual(interm_sqs, [
@@ -50,8 +35,7 @@ class MotionTestCase(unittest.TestCase):
             ('c', 1),
             ('d', 1),
             ('e', 1),
-            ('f', 1),
-            ('g', 1)
+            ('f', 1)
         ])
 
         interm_sqs = RookMotion.intermediate_squares(('g',1), ('a', 1))
@@ -60,12 +44,142 @@ class MotionTestCase(unittest.TestCase):
             ('e', 1),
             ('d', 1),
             ('c', 1),
-            ('b', 1),
-            ('a', 1)
+            ('b', 1)
         ])
 
+    def test_rook_intermediate_squares_will_be_0(self):
+        interm_sqs = RookMotion.intermediate_squares(('a', 1), ('a', 2))
+        self.assertEqual(0, len(interm_sqs))
 
-class MotionLanguageTestCase(unittest.TestCase):
-    def test_initialize_ml(self):
-        test_rook_motion = 'N(+-)*'
-        rook_motion = MotionLanguage(test_rook_motion)
+
+    def test_valid_move(self):
+        can_move_to_square = RookMotion.valid_move('a1', 'a8')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = RookMotion.valid_move('a1', 'h8')
+        self.assertFalse(can_move_to_square)
+
+class BishopMotionTestCase(unittest.TestCase):
+    def test_valid_move(self):
+        can_move_to_square = BishopMotion.valid_move('a1', 'h8')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = BishopMotion.valid_move('a8', 'h1')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = BishopMotion.valid_move('a1', 'a8')
+        self.assertFalse(can_move_to_square)
+        can_move_to_square = BishopMotion.valid_move('d6', 'g3')
+        self.assertTrue(can_move_to_square)
+
+    def test_bishop_intermediate_squares_will_be_0(self):
+        interm_sqs = BishopMotion.intermediate_squares(('a', 1), ('b', 2))
+        self.assertEqual(0, len(interm_sqs))
+
+    def test_bishop_intermediate_squares(self):
+        interm_sqs = BishopMotion.intermediate_squares(('a', 1), ('h', 8))
+        self.assertEqual(interm_sqs, [
+            ('b', 2),
+            ('c', 3),
+            ('d', 4),
+            ('e', 5),
+            ('f', 6),
+            ('g', 7)
+        ])
+        interm_sqs = BishopMotion.intermediate_squares(('b', 7), ('g', 2))
+        self.assertEqual(interm_sqs, [
+            ('c', 6),
+            ('d', 5),
+            ('e', 4),
+            ('f', 3),
+        ])
+        
+class QueenMotionTestCase(unittest.TestCase):
+    def test_valid_move(self):
+        can_move_to_square = QueenMotion.valid_move('a1', 'h8')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = QueenMotion.valid_move('a1', 'h7')
+        self.assertFalse(can_move_to_square)
+
+    def test_queen_intermediate_squares(self):
+        interm_sqs = QueenMotion.intermediate_squares(('a', 1), ('h', 8))
+        self.assertEqual(interm_sqs, [
+            ('b', 2),
+            ('c', 3),
+            ('d', 4),
+            ('e', 5),
+            ('f', 6),
+            ('g', 7)
+        ])
+        interm_sqs = QueenMotion.intermediate_squares(('a', 1), ('a', 8))
+        self.assertEqual(interm_sqs, [
+            ('a', 2), 
+            ('a', 3),
+            ('a', 4),
+            ('a', 5),
+            ('a', 6),
+            ('a', 7)
+        ])
+        interm_sqs = QueenMotion.intermediate_squares(('a',1), ('g', 1))
+        self.assertEqual(interm_sqs, [
+            ('b', 1),
+            ('c', 1),
+            ('d', 1),
+            ('e', 1),
+            ('f', 1)
+        ])
+
+class KingMotionTestCase(unittest.TestCase):
+    def test_valid_move(self):
+        can_move_to_square = KingMotion.valid_move('a1', 'b2')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = KingMotion.valid_move('a1', 'a8')
+        self.assertFalse(can_move_to_square)
+
+    def test_king_intermediate_squares(self):
+        interm_sqs = KingMotion.intermediate_squares(('a', 1), ('a', 2))
+        self.assertEqual(interm_sqs, [ ])
+        with self.assertRaises(AssertionError):
+            interm_sqs = KingMotion.intermediate_squares(('a', 1), ('a', 8))
+
+
+class KnightMotionTestCase(unittest.TestCase):
+    def test_valid_move(self):
+        can_move_to_square = KnightMotion.valid_move('a1', 'b3')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = KnightMotion.valid_move('e4', 'd2')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = KnightMotion.valid_move('e4', 'e5')
+        self.assertFalse(can_move_to_square)
+
+    def test_king_intermediate_squares(self):
+        interm_sqs = KnightMotion.intermediate_squares(('a', 1), ('c', 2))
+        self.assertEqual(interm_sqs, [ ])
+        with self.assertRaises(AssertionError):
+            interm_sqs = KingMotion.intermediate_squares(('a', 1), ('a', 8))
+
+
+class PawnMotionTestCase(unittest.TestCase):
+    def test_valid_first_move(self):
+        can_move_to_square = PawnMotion.valid_move('a2', 'a3', first_move=True)
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = PawnMotion.valid_move('e2', 'e4', first_move=True)
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = PawnMotion.valid_move('a2', 'a5', first_move=True)
+        self.assertFalse(can_move_to_square)
+
+    def test_capture(self):
+        can_move_to_square = PawnMotion.valid_move('a3', 'b4', capture=True)
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = PawnMotion.valid_move('a3', 'c4', capture=True)
+        self.assertFalse(can_move_to_square)
+
+    def test_valid_move(self):
+        can_move_to_square = PawnMotion.valid_move('c3', 'c4')
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = PawnMotion.valid_move('c3', 'c5')
+        self.assertFalse(can_move_to_square)
+
+    def test_valid_capture(self):
+        can_move_to_square = PawnMotion.valid_move('c3', 'd4', capture=True)
+        self.assertTrue(can_move_to_square)
+        can_move_to_square = PawnMotion.valid_move('c3', 'c4', capture=True)
+        self.assertFalse(can_move_to_square)
+
